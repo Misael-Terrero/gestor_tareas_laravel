0bd933ba9135e331e64bf88c6fb3fa0b8a93124c
Misael Terrero <misaelterrero96@gmail.com>
2025-09-08 16:39:21 -0400

    agregando las rutas necesarias para mostrar las tareas

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,30 @@
 <?php
-
+use App\Http\Controllers\TareaController;
+use App\Models\Tarea;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+# ------------------------------------------------------------
+# Rutas para las vistas de tareas
+Route::get('/tareas', [TareaController::class, 'index'])->name('tareas.index');
+Route::get('/tareas/create', [TareaController::class, 'create'])->name('tareas.create');
+Route::post('/tareas', [TareaController::class, 'store'])->name('tareas.store');
+Route::get('/tareas/{id}/edit', [TareaController::class, 'edit'])->name('tareas.edit');
+Route::put('/tareas/{id}', [TareaController::class, 'update'])->name('tareas.update');
+Route::delete('/tareas/{id}', [TareaController::class, 'destroy'])->name('tareas.destroy');
+
+
+
+Route::get('/dashboard', function () {
+    $tareas = Tarea::latest()->take(5)->get(); // Las 5 tareas más recientes
+    return view('dashboard', compact('tareas'));
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+# ------------------------------------------------------------
 
 Route::get('/dashboard', function () {
     return view('dashboard');
