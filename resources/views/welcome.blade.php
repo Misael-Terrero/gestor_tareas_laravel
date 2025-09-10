923fa2ad526a6a6fae3c7badec2e71150f576d46
Misael Terrero <misaelterrero96@gmail.com>
2025-09-10 12:32:14 -0400

    reutilizando el logo para wel archivo weelcome y login, logo esta en application

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,37 +124,8 @@
                 
                 <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
                     {{-- Laravel Logo --}}
-                    <div style="display: flex; justify-content: center; align-items: center; height: 100%; width: 100%;">
-                        <!-- Aquí va tu SVG -->
-                         <svg width="60%" height="auto" viewBox="0 0 300 120" xmlns="http://www.w3.org/2000/svg">
-                        <!-- Fondo cuadrado teal -->
-                        <rect x="0" y="0" width="110" height="120" rx="20" fill="#2CB5A0"/>
-
-                        <!-- Clipboard -->
-                        <rect x="15" y="30" width="80" height="80" rx="6" fill="white"/>
-                        <rect x="30" y="25" width="50" height="10" rx="3" fill="#2CB5A0"/>
-
-                        <!-- Checkmark -->
-                        <path d="M50 45 L58 53 L70 40" stroke="#2CB5A0" stroke-width="4" fill="none" stroke-linecap="round"/>
-
-                        <!-- Líneas de tareas -->
-                        <line x1="25" y1="60" x2="85" y2="60" stroke="#2CB5A0" stroke-width="3" stroke-linecap="round"/>
-                        <line x1="25" y1="70" x2="85" y2="70" stroke="#2CB5A0" stroke-width="3" stroke-linecap="round"/>
-                        <line x1="25" y1="80" x2="85" y2="80" stroke="#2CB5A0" stroke-width="3" stroke-linecap="round"/>
-                        <line x1="25" y1="90" x2="85" y2="90" stroke="#2CB5A0" stroke-width="3" stroke-linecap="round"/>
-
-                        <!-- Texto del nombre del proyecto -->
-                        <text x="120" y="40" font-family="Arial, sans-serif" font-size="41" fill="#e8e8e8ff" font-weight="bold">
-                            Smil
-                        </text>
-                        <text x="120" y="73" font-family="Arial, sans-serif" font-size="32" fill="#e8e8e8ff" font-weight="bold">
-                            Project
-                        </text>
-                        <text x="120" y="110" font-family="Arial, sans-serif" font-size="25" fill="#e7e4e4ff">
-                            Task Management
-                        </text>
-                        </svg>
-                    </div>
+                    <x-application-logo />
+                    
                     <div class="absolute inset-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]"></div>
                 </div>
             </main>
