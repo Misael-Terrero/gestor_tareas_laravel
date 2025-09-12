2e8dba5509ad6c41a05a4b0d35d098a194fb7f75
Misael Terrero <misaelterrero96@gmail.com>
2025-09-12 09:46:35 -0400

    mejorando diseño

--- resources/views/tareas/edit.blade.php
+++ resources/views/tareas/edit.blade.php
@@ -28,8 +28,8 @@
                         <label for="completada" class="inline-flex items-center">
                             <input type="checkbox" name="completada" id="completada" value="1"
                                    {{ $tarea->completada ? 'checked' : '' }}
-                                   class="rounded">
-                            <span class="ml-2 text-gray-700 dark:text-gray-300">{{ __("Completed?") }}</span>
+                                   class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800">
+                            <span class="ms-2 text-sm text-gray-600 dark:text-gray-300">{{ __("Completed?") }}</span>
                         </label>
                     </div>
 
